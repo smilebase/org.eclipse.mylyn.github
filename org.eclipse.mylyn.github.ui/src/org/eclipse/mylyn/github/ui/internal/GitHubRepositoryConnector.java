9e97f7a9bde9a38b4051c7282685a63c78b6d84e
Brian Gianforcaro <b.gianfo@gmail.com>
2009-08-24 00:43:10 -0400

    Took a crack at connecting the Service to the UI for "hasTaskChanged" and "postTask". Needs more testing

--- org.eclipse.mylyn.github.ui/src/org/eclipse/mylyn/github/ui/internal/GitHubRepositoryConnector.java
+++ org.eclipse.mylyn.github.ui/src/org/eclipse/mylyn/github/ui/internal/GitHubRepositoryConnector.java
@@ -42,9 +42,6 @@ import org.eclipse.mylyn.tasks.core.sync.ISynchronizationSession;
 
 /**
  * GitHub connector specific extensions.
- * 
- * @author Christian Trutz
- * @since 0.1.0
  */
 public class GitHubRepositoryConnector extends AbstractRepositoryConnector {
 
@@ -68,7 +65,7 @@ public class GitHubRepositoryConnector extends AbstractRepositoryConnector {
 	}
 
 	/**
-	 * Get the connector kind.
+	 * Static getter for the connector kind.
 	 * 
 	 * @return The connector kind as a string
 	 */
@@ -126,29 +123,75 @@ public class GitHubRepositoryConnector extends AbstractRepositoryConnector {
 	}
 
 	@Override
-	public TaskData getTaskData(TaskRepository taskRepository, String taskId,
+	public TaskData getTaskData(TaskRepository repo, String taskId,
 			IProgressMonitor monitor) throws CoreException {
-		TaskAttributeMapper taskAttributeMapper = new TaskAttributeMapper(
-				taskRepository);
-		return new TaskData(taskAttributeMapper, taskRepository
-				.getConnectorKind(), taskRepository.getRepositoryUrl(), taskId);
+
+		TaskAttributeMapper mapper;
+		mapper = new TaskAttributeMapper(repo);
+
+		String kind = repo.getConnectorKind();
+		String url = repo.getRepositoryUrl();
+
+		return new TaskData(mapper, kind, url, taskId);
 	}
 
+	/** Not used because with github we can construct these on our own. */
 	@Override
 	public String getTaskIdFromTaskUrl(String taskFullUrl) {
 		return null;
 	}
 
+	/** Note used because with github we can construct these on our own */
 	@Override
 	public String getTaskUrl(String repositoryUrl, String taskId) {
 		return null;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
-	public boolean hasTaskChanged(TaskRepository taskRepository, ITask task,
+	public boolean hasTaskChanged(TaskRepository repository, ITask task,
 			TaskData taskData) {
-		return true;
+		GitHubIssue issue = new GitHubIssue();
+
+		TaskAttribute root = taskData.getRoot();
+		TaskAttribute title = root.getAttribute(TaskAttribute.SUMMARY);
+		TaskAttribute body = root.getAttribute(TaskAttribute.DESCRIPTION);
+
+		String titleStr = title.getValue();
+		String bodyStr = body.getValue();
+		String userName = repository.getUserName();
+		String userStr = repository.getUserName();
+		String idStr = taskData.getTaskId();
+		String repo = repository.getRepositoryLabel();
+
+		issue.setBody(bodyStr);
+		issue.setTitle(titleStr);
+		issue.setUser(userStr);
+		issue.setNumber(idStr);
+
+		GitHubIssues issues = null;
+		try {
+			issues = service.searchIssues(userName, repo, "open", titleStr);
+		} catch (GitHubServiceException e) {
+			return true;
+		}
+
+		boolean result = true;
+		// Make check if all the values are the same.
+		if (issues != null) {
+			for (GitHubIssue isu : issues.getIssues()) {
+				if (isu.getNumber().equals(idStr)) {
+					result = isu.getBody().equals(bodyStr)
+							&& isu.getTitle().equals(titleStr)
+							&& issue.getUser().equals(userName);
+					break;
+				}
+			}
+		}
+		return !result;
 	}
+
+	/** Not real sure what this is for */
 
 	@Override
 	public void updateRepositoryConfiguration(TaskRepository taskRepository,
@@ -158,6 +201,7 @@ public class GitHubRepositoryConnector extends AbstractRepositoryConnector {
 	@Override
 	public void updateTaskFromTaskData(TaskRepository taskRepository,
 			ITask task, TaskData taskData) {
+
 		ITaskMapping taskMapping = getTaskMapping(taskData);
 		task.setSummary(taskMapping.getSummary());
 	}
@@ -180,14 +224,58 @@ public class GitHubRepositoryConnector extends AbstractRepositoryConnector {
 				return new TaskAttributeMapper(taskRepository);
 			}
 
+			@SuppressWarnings("deprecation")
 			@Override
 			public RepositoryResponse postTaskData(TaskRepository repository,
 					TaskData taskData, Set<TaskAttribute> oldAttributes,
 					IProgressMonitor monitor) throws CoreException {
-				return new RepositoryResponse(ResponseKind.TASK_CREATED,
-						"ID12345");
+
+				GitHubIssue issue = new GitHubIssue();
+
+				TaskAttribute root = taskData.getRoot();
+				TaskAttribute title = root.getAttribute(TaskAttribute.SUMMARY);
+				TaskAttribute body = root
+						.getAttribute(TaskAttribute.DESCRIPTION);
+
+				String userName = repository.getUserName();
+				String apiKey = repository.getHttpPassword();
+				String titleStr = title.getValue();
+				String bodyStr = body.getValue();
+				String userStr = repository.getUserName();
+				String idStr = taskData.getTaskId();
+				String repo = repository.getRepositoryLabel();
+
+				issue.setBody(bodyStr);
+				issue.setTitle(titleStr);
+				issue.setUser(userStr);
+				issue.setNumber(idStr);
+
+				boolean result;
+
+				try {
+					if (taskData.isNew()) {
+						result = service.openIssue(userName, repo, issue,
+								apiKey);
+					} else {
+						result = service.editIssue(userName, repo, issue,
+								apiKey);
+					}
+				} catch (GitHubServiceException e) {
+					IStatus status = Status.CANCEL_STATUS;
+					CoreException up = new CoreException(status);
+					throw up;
+				}
+
+				if (result) {
+					return new RepositoryResponse(ResponseKind.TASK_CREATED,
+							taskData.getTaskId());
+				}
+				// Error occurred when trying to open the issue.
+				IStatus status = Status.CANCEL_STATUS;
+				CoreException up = new CoreException(status);
+				throw up;
+
 			}
 		};
 	}
-
 }
